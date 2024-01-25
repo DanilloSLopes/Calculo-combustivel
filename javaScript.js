3ecca99c646b9970477d77760f66ef9eeb1f813b
Danillo Lopes <danillosantiagolopes@gmail.com>
2024-01-25 20:17:17 -0300

    Criação do botão reset, e das classes

--- javaScript.js
+++ javaScript.js
@@ -4,15 +4,19 @@ var consumo = document.getElementById('consumo')
 var calculo = document.getElementById('altera')
 
 function calculoConsumo(km, combustivel, consumo){
-    let kmAtt = parseFloat((km.value).replace("," , "."))
-    let consumoAtt = parseFloat((consumo.value).replace("," , "."))
-    let combustivelAtt = parseFloat((combustivel.value).replace("," , "."))
+    if(km.value.length == 0 || combustivel.value.length == 0 || consumo.value.length == 0){
+        window.alert('Preencha os valores para realização do calculo')
+    } else {
+        let kmAtt = parseFloat((km.value).replace("," , "."))
+        let consumoAtt = parseFloat((consumo.value).replace("," , "."))
+        let combustivelAtt = parseFloat((combustivel.value).replace("," , "."))
 
-    let res = (kmAtt / consumoAtt * combustivelAtt)
-    let resAtt = res.toFixed(2)
-    let resFormatada = (String(resAtt)).replace(".", ",")
+        let res = (kmAtt / consumoAtt * combustivelAtt)
+        let resAtt = res.toFixed(2)
+        let resFormatada = (String(resAtt)).replace(".", ",")
 
-    calculo.innerHTML = `O valor gasto na viajem é de R$${resFormatada}`
+        calculo.innerHTML = `O valor gasto na viagem é de R$${resFormatada}`
+    }  
 }
 
 //Fazendo a função para o botao reset
@@ -21,3 +25,17 @@ resetar.addEventListener("click", () => {
     location.reload()
 } )
 
+//Criando a Classe para acessar os carros pré cadastrados
+class Carros {
+    constructor(modelo, consumoG, consumoA){
+        this.modelo = modelo
+        this.consumoG = consumoG
+        this.consumoA = consumoA
+    }
+}
+
+const Kwid = new Carros('Kwid', '15', '12')
+const Gol = new Carros('Gol', '11', '9')
+const Fiesta = new Carros('Fiesta', '13', '10')
+
+var modCarros = [Kwid, Gol, Fiesta]
